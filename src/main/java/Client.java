36b854754a64bcba118cb2504c4665a857714c9e
zhaochong <1060787856@qq,com>
2017-11-07 21:20:23 +0800

    提交查分代码

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,7 +4,13 @@
 
 public class Client {
     public static void main(String[] args) {
-        String apkPath = "C:\\Users\\ecarx\\Desktop\\coolqiv2.1.9_219_jiagu_sign.apk";
-        AppCreateJsonTool.readApk(args[0]);
+       /* String apkPath = "C:\\Users\\ecarx\\Desktop\\coolqiv2.1.9_219_jiagu_sign.apk";
+        AppCreateJsonTool.readApk(args[0]);*/
+        try {
+            String cmdStr1="bsdiff.exe  app-old.apk app-new.apk  com.ecarx.glautoupdatesdk.gnetlink_1_400_ECARX.patch";
+            Runtime.getRuntime().exec("cmd.exe /c start "+cmdStr1);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
